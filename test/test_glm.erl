19d0be8f734842222a0884c4246fe8277a9131a2
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2025-09-08 13:49:02 +0800

    Implement assert scalar functions in test_glm.erl

--- test/test_glm.erl
+++ test/test_glm.erl
@@ -11,6 +11,123 @@
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("glm/include/glm.hrl").
 
+-export([
+    assert_bool/2,
+    assert_int8/2,
+    assert_int16/2,
+    assert_int32/2,
+    assert_int64/2,
+    assert_uint8/2,
+    assert_uint16/2,
+    assert_uint32/2,
+    assert_uint64/2,
+    assert_float/2,
+    assert_double/2
+]).
+-export([
+    assert_scalar/2,
+    assert_scalar/3
+]).
+
+assert_bool(S, V) ->
+    {scalar, bool, D} = S,
+    ?assert(erlang:is_binary(D)),
+    ok = assert_byte_size(bool, 1, D),
+    ?assertEqual(V, glm:bool_value(S)),
+    ok.
+
+assert_int8(S, V) ->
+    {scalar, {int, 8}, D} = S,
+    ?assert(erlang:is_binary(D)),
+    ok = assert_byte_size({int, 8}, 1, D),
+    ?assertEqual(V, glm:int8_value(S)),
+    ok.
+
+assert_int16(S, V) ->
+    {scalar, {int, 16}, D} = S,
+    ?assert(erlang:is_binary(D)),
+    ok = assert_byte_size({int, 16}, 1, D),
+    ?assertEqual(V, glm:int16_value(S)),
+    ok.
+
+assert_int32(S, V) ->
+    {scalar, {int, 32}, D} = S,
+    ?assert(erlang:is_binary(D)),
+    ok = assert_byte_size({int, 32}, 1, D),
+    ?assertEqual(V, glm:int32_value(S)),
+    ok.
+
+assert_int64(S, V) ->
+    {scalar, {int, 64}, D} = S,
+    ?assert(erlang:is_binary(D)),
+    ok = assert_byte_size({int, 64}, 1, D),
+    ?assertEqual(V, glm:int64_value(S)),
+    ok.
+
+assert_uint8(S, V) ->
+    {scalar, {uint, 8}, D} = S,
+    ?assert(erlang:is_binary(D)),
+    ok = assert_byte_size({uint, 8}, 1, D),
+    ?assertEqual(V, glm:uint8_value(S)),
+    ok.
+
+assert_uint16(S, V) ->
+    {scalar, {uint, 16}, D} = S,
+    ?assert(erlang:is_binary(D)),
+    ok = assert_byte_size({uint, 16}, 1, D),
+    ?assertEqual(V, glm:uint16_value(S)),
+    ok.
+
+assert_uint32(S, V) ->
+    {scalar, {uint, 32}, D} = S,
+    ?assert(erlang:is_binary(D)),
+    ok = assert_byte_size({uint, 32}, 1, D),
+    ?assertEqual(V, glm:uint32_value(S)),
+    ok.
+
+assert_uint64(S, V) ->
+    {scalar, {uint, 64}, D} = S,
+    ?assert(erlang:is_binary(D)),
+    ok = assert_byte_size({uint, 64}, 1, D),
+    ?assertEqual(V, glm:uint64_value(S)),
+    ok.
+
+assert_float(S, V) ->
+    {scalar, float, D} = S,
+    ?assert(erlang:is_binary(D)),
+    ok = assert_byte_size(float, 1, D),
+    ?assert(erlang:abs(glm:float_value(S) - V) < 1.0e-9),
+    ok.
+
+assert_double(S, V) ->
+    {scalar, double, D} = S,
+    ?assert(erlang:is_binary(D)),
+    ok = assert_byte_size(double, 1, D),
+    ?assert(erlang:abs(glm:double_value(S) - V) < 1.0e-12),
+    ok.
+
+assert_scalar(T, V) ->
+    {scalar, T, D} = V,
+    ?assert(erlang:is_binary(D)),
+    ok = assert_byte_size(T, 1, D),
+
+    ok.
+
+assert_scalar(T, V, Expected) ->
+    {scalar, T, D} = V,
+    ?assert(erlang:is_binary(D)),
+    ok = assert_byte_size(T, 1, D),
+    case T of
+        float ->
+            ?assert(erlang:abs(glm:float_value(V) - Expected) < 1.0e-9);
+        double ->
+            ?assert(erlang:abs(glm:double_value(V) - Expected) < 1.0e-12);
+        _ ->
+            ?assertEqual(Expected, glm:scalar_value(V))
+    end,
+
+    ok.
+
 assert_byte_size(T, N, D) ->
     ByteSize = case T of
         bool -> ?BOOL_BYTE_SIZE;
