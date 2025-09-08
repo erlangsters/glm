6236538779f7b4c3aa606a9b1b2ecd9329a4780a
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2025-09-08 13:50:41 +0800

    reword - Implement valid/invalid value generators in test_glm.erl

--- test/test_glm.erl
+++ test/test_glm.erl
@@ -8,9 +8,14 @@
 %% Written by Jonathan De Wachter <jonathan.dewachter@byteplug.io>
 %%
 -module(test_glm).
+-include_lib("proper/include/proper.hrl").
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("glm/include/glm.hrl").
 
+-export([
+    valid_value/1,
+    invalid_value/1
+]).
 -export([
     assert_bool/2,
     assert_int8/2,
@@ -28,6 +33,123 @@
     assert_scalar/2,
     assert_scalar/3
 ]).
+-export([
+    assert_vec2/3,
+    assert_vec3/3,
+    assert_vec4/3
+]).
+-export([
+    assert_vec/3,
+    assert_vec/4
+]).
+
+valid_value(bool) ->
+    boolean();
+valid_value({int, 8}) ->
+    integer(-128, 127);
+valid_value({int, 16}) ->
+    integer(-32768, 32767);
+valid_value({int, 32}) ->
+    integer(-2147483648, 2147483647);
+valid_value({int, 64}) ->
+    integer(-9223372036854775808, 9223372036854775807);
+valid_value({uint, 8}) ->
+    integer(0, 255);
+valid_value({uint, 16}) ->
+    integer(0, 65535);
+valid_value({uint, 32}) ->
+    integer(0, 4294967295);
+valid_value({uint, 64}) ->
+    integer(0, 18446744073709551615);
+valid_value(float) ->
+    oneof([
+        0.0,
+        -0.0,
+        1.0,
+        -1.0,
+        1.175494e-38,
+        -1.175494e-38
+    ]);
+valid_value(double) ->
+    oneof([
+        0.0,
+        -0.0,
+        1.0,
+        -1.0,
+        3.4028235e+38,
+        -3.4028235e+38,
+        1.7976931348623157e+308,
+        -1.7976931348623157e+308
+    ]).
+
+invalid_value(bool) ->
+    oneof([integer(), float()]);
+invalid_value({int, 8}) ->
+    oneof([
+        boolean(),
+        integer(128, 1000),
+        integer(-1000, -129),
+        float()
+    ]);
+invalid_value({int, 16}) ->
+    oneof([
+        boolean(),
+        integer(32768, 100000),
+        integer(-100000, -32769),
+        float()
+    ]);
+invalid_value({int, 32}) ->
+    oneof([
+        boolean(),
+        integer(2147483648, 10000000000),
+        integer(-10000000000, -2147483649),
+        float()
+    ]);
+invalid_value({int, 64}) ->
+    oneof([
+        boolean(),
+        -9223372036854775809, % Just below min (-2^63 - 1)
+        9223372036854775808,  % Just above max (2^63)
+        float()
+    ]);
+invalid_value({uint, 8}) ->
+    oneof([
+        boolean(),
+        integer(-1000, -1),
+        integer(256, 1000),
+        float()
+    ]);
+invalid_value({uint, 16}) ->
+    oneof([
+        boolean(),
+        integer(-1000, -1),
+        integer(65536, 100000),
+        float()
+    ]);
+invalid_value({uint, 32}) ->
+    oneof([
+        boolean(),
+        integer(-10000000000, -1),
+        integer(4294967296, 10000000000),
+        float()
+    ]);
+invalid_value({uint, 64}) ->
+    oneof([
+        boolean(),
+        -1,                   % Just below min
+        18446744073709551616, % Just above max (2^64)
+        float()
+    ]);
+invalid_value(float) ->
+    oneof([
+        boolean(),
+        integer()
+    ]);
+invalid_value(double) ->
+    oneof([
+        boolean(),
+        integer()
+    ]).
 
 assert_bool(S, V) ->
     {scalar, bool, D} = S,
